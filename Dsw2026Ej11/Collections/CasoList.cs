bb580fb9699d38f0a718e1e909e850f4bf422390
Nicolas Perry <nicolasperry04@gmail.com>
2026-05-20 20:37:07 -0300

    Completo la clase CasoList

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using System.Globalization;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +11,32 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> alumnos;
+
+    public CasoList() {
+
+        alumnos = new List<Alumno>();
+    }
+
+    public void AgregarAlum(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+
+    public List<Alumno> RetornarList()
+    {
+        return alumnos;
+    }
+
+    public Alumno BuscarAlumno(int id)
+    {
+        foreach (var alumno in alumnos)
+        {
+            if (alumno.Id == id)
+            {
+                return alumno;
+            }
+        }
+
+    }
 }
