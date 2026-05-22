c46fb2d29c1e032ea1aad04481ff2e755003002c
Nicolas Perry <nicolasperry04@gmail.com>
2026-05-22 15:54:23 -0300

    Completo el CasoLinq y completo los ejemplos de prueba

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -49,12 +49,16 @@ internal class Ejemplos
             Console.WriteLine("No existe");
         }
 
+        Console.WriteLine("\n Eliminando Alumno 2");
+
         lista.EliminarAlumno(alumno2);
 
         foreach (Alumno alumno in lista.RetornarList())
         {
             Console.WriteLine("Id: " + alumno.Id + " Nombre: " + alumno.Nombre + " Promedio: " + alumno.Promedio);
         }
+
+        Console.WriteLine("Eliminando el alumno de la poscion 0");
 
         lista.EliminacionPorPosicion(0);
 
@@ -72,12 +76,106 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        CasoDictionary diccionario = new CasoDictionary();
+        Alumno alumno1 = new Alumno(45, "Nico", 8);
+        Alumno alumno2 = new Alumno(66, "Juan", 5);
+        Alumno alumno3 = new Alumno(18, "Lucas", 9);
 
+        diccionario.AgregarAlumno(60680, alumno1);
+        diccionario.AgregarAlumno(1111, alumno2);
+        diccionario.AgregarAlumno(22222, alumno3);
+
+
+        foreach (var alumno in diccionario.RetornarDictionary())
+        {
+            Console.WriteLine("Legajo: " + alumno.Key + " Id: " + alumno.Value.Id + " Nombre: " + alumno.Value.Nombre + " Promedio: " + alumno.Value.Promedio);
+        }
+
+        Alumno buscado = diccionario.BuscarAlumno(60680);
+
+        Console.WriteLine("\n Busco el alumno con Id: " + buscado.Id);
+
+        if (buscado != null)
+        {
+            Console.WriteLine("Alumno encontrado: " + buscado.Nombre);
+        }
+
+        Console.WriteLine("\n Buscando legajo inexistente");
+
+        Alumno buscado2 = diccionario.BuscarAlumno(9999);
+
+        if (buscado2 != null)
+        {
+            Console.WriteLine("Alumno encontrado: " + buscado2.Nombre);
+        }
+        else
+        {
+            Console.WriteLine("No existe");
+        }
+
+        Console.WriteLine("\n Elimino al alumno de legajo 1111");
+
+        diccionario.EliminarAlumno(1111);
+
+        foreach (var alumno in diccionario.RetornarDictionary())
+        {
+            Console.WriteLine("Legajo: " + alumno.Key + " Id: " + alumno.Value.Id + " Nombre: " + alumno.Value.Nombre + " Promedio: " + alumno.Value.Promedio);
+        }
     }
 
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
+        CasoLinq caso = new CasoLinq();
+
+        // 1
+        Console.WriteLine("Primer libro:");
+        Console.WriteLine(caso.GetPrimero(null).Titulo);
+
+        // 2
+        Console.WriteLine("\nÚltimo libro:");
+        Console.WriteLine(caso.GetUltimo().Titulo);
+
+        // 3
+        Console.WriteLine("\nSuma total de precios:");
+        Console.WriteLine(caso.GetTotalPrecios());
+
+        // 4
+        Console.WriteLine("\nPromedio de precios:");
+        Console.WriteLine(caso.GetPromedioPrecios());
+
+        // 5
+        Console.WriteLine("\nLibros con ID mayor a 15:");
+
+        foreach (var libro in caso.ListPorId())
+        {
+            Console.WriteLine(libro.Titulo);
+        }
+
+        // 7
+        Console.WriteLine("\nLibro con mayor precio:");
+        Console.WriteLine(caso.GetMayorPrecio().Titulo);
+
+        // 8
+        Console.WriteLine("\nLibro con menor precio:");
+        Console.WriteLine(caso.GetMenorPrecio().Titulo);
+
+        // 9
+        Console.WriteLine("\nLibros con precio mayor al promedio:");
+
+        foreach (var libro in caso.GetMayorPromedio())
+        {
+            Console.WriteLine(libro.Titulo + " - " + libro.Precio);
+        }
+
+        // 10
+        Console.WriteLine("\nLibros ordenados descendentemente:");
+
+        foreach (var libro in caso.OrdenarDesc())
+        {
+            Console.WriteLine(libro.Titulo);
+        }
 
     }
 }
+
